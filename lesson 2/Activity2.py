0bdae4e1670adc5457d2d72793346b20581c157a
moharana6780-dev <moharana6780@gmail.com>
2026-04-22 20:31:50 +0300

    okay

---
+++ lesson 2/Activity2.py
@@ -0,0 +1,22 @@
+
+
+
+MyName = input("Enter Your Name")
+
+
+print("My name is ",MyName ,"I am a good boy" ) 
+
+Myage = input("Enter your age")
+
+
+
+
+
+print("My Age Is ",Myage , "years old")
+
+
+
+1name = 
+
+
+My_Name 
---
+++ lesson 2/Activityone.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+
+number =  10 
+
+
+numberTwo = 200000
+
+
+
+print(numberTwo)
+
+
+numbersix = 60000000000000000
+
+MyName = ""
+
+print(numbersix) 
+MyName = "Aayush"
+print(MyName)
