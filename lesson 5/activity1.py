705e8393e6486ecb3809710f238c324173f42787
moharana6780-dev <moharana6780@gmail.com>
2026-05-10 20:37:04 +0300

    okay

---
+++ lesson 5/activity1.py
@@ -0,0 +1,11 @@
+heightone = 66.5
+heighttwo = 66.5
+heightthree = 66.5
+heightfour = 66.5
+heightfive = 66.5 
+SUM = heightone + heighttwo + heightthree + heightfour + heightfive
+print(SUM)
+AVERAGE = SUM / 5 
+print(AVERAGE)
+
+
---
+++ lesson 5/activity3.py
@@ -0,0 +1,14 @@
+English = 76
+Maths = 70
+Hindi = 68
+Sanskrit = 52 
+Biology = 90
+Physics = 79
+Chemistry = 75 
+
+SUM = English + Maths + Hindi + Sanskrit + Biology + Physics + Chemistry
+print(SUM)
+
+AVERAGE = SUM / 700
+PERCENTAGE = AVERAGE * 100 
+print(PERCENTAGE,"is your marks")
